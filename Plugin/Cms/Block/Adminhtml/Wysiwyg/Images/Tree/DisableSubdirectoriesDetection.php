31341a5739650dc05fbb00fc67fefa764d485948
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2021-04-20 07:40:03 +0000

    fix(media-gallery): removed calculation of subfolders status to improve performance of folders tree generation

---
+++ Plugin/Cms/Block/Adminhtml/Wysiwyg/Images/Tree/DisableSubdirectoriesDetection.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace MageSuite\S3MediaListing\Plugin\Cms\Block\Adminhtml\Wysiwyg\Images\Tree;
+
+// Detecting whether directory has subdirectories introduced for media files tree in
+// Magento >= 2.4.0 requires a lot of additional requests done to S3 API = performance hit
+// This plugin disables calculation for improved performance
+// Downside is that all folders in tree are displayed with arrow to show subfolders even
+// when in reality they don't have any subfolders
+class DisableSubdirectoriesDetection
+{
+    /**
+     * @var \Magento\Cms\Helper\Wysiwyg\Images
+     */
+    protected $cmsWysiwygImages;
+
+    /**
+     * @var \Magento\Framework\Registry
+     */
+    protected $registry;
+
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
+    protected $serializer;
+
+    public function __construct(
+        \Magento\Cms\Helper\Wysiwyg\Images $cmsWysiwygImages,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Serialize\Serializer\Json $serializer
+    )
+    {
+        $this->cmsWysiwygImages = $cmsWysiwygImages;
+        $this->registry = $registry;
+        $this->serializer = $serializer;
+    }
+
+    public function aroundGetTreeJson(
+        \Magento\Cms\Block\Adminhtml\Wysiwyg\Images\Tree $subject,
+        callable $proceed
+    )
+    {
+        $storageRoot = $this->cmsWysiwygImages->getStorageRoot();
+
+        $collection = $this->registry
+            ->registry('storage')
+            ->getDirsCollection($this->cmsWysiwygImages->getCurrentPath());
+
+        $jsonArray = [];
+
+        foreach ($collection as $item) {
+            $data = [
+                'text' => $this->cmsWysiwygImages->getShortFilename($item->getBasename(), 20),
+                'id' => $this->cmsWysiwygImages->convertPathToId($item->getFilename()),
+                'path' => substr($item->getFilename(), strlen($storageRoot)),
+                'cls' => 'folder',
+            ];
+
+            $jsonArray[] = $data;
+        }
+
+        return $this->serializer->serialize($jsonArray);
+    }
+}
